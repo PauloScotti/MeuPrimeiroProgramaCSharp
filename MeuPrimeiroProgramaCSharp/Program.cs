fcd0084bc712a0427fcb2d68ff335929eddcdca8
Paulo Scotti <paulo.scotti@hotmail.com>
2023-11-27 10:18:46 -0300

    Adicionar arquivos de projeto.

---
+++ MeuPrimeiroProgramaCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿string nomecompleto;
+string textoolamundo = "Olá mundo, meu primeiro programa CSharp! Feito por ";
+int idade;
+
+Console.WriteLine("Qual o seu nome?");
+
+nomecompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual a sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(textoolamundo + nomecompleto + " que tem " + idade + " anos.");
